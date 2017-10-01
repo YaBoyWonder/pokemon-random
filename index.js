c5ffe24fad73656d9e62f14e75c61ac22ef2f36a
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-09-30 21:18:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pokemon1 = require('./randomPokemon')()
-const pokemon2 = require('./randomPokemon')()
+const pokemon2 = require('./randomPokemon')() 
  
 console.log('pokemon1', pokemon1)
 console.log('pokemon2', pokemon2)
