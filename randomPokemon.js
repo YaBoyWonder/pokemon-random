cfb0a61abdc817ae16e1a42f9397ac8d1ef8ea21
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2018-08-20 06:20:35 -0700

    Delete randomPokemon.js

--- randomPokemon.js
+++
@@ -1,6 +0,0 @@
-const Pokemons = require('./src/pokemonRow.json')
-
-module.exports = () => {
-  const randomPokemon = (total) => (Math.random() * total | 0) + 1
-  return Pokemons[randomPokemon(Pokemons.length)]
-}
