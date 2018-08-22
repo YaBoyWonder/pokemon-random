7ab2946df61ba54d99c90c06f0d4cc955778abfc
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2018-08-21 18:21:34 -0700

    upload

---
+++ api/README.md
@@ -0,0 +1,9 @@
+<div align="center">
+  <br />
+  <p>
+    <a href="https://www.npmjs.com/package/pokemon-random"><img src="http://i.imgur.com/bnO3Tct.png" width="546" alt="discord.js" /></a>
+  </p>
+  <br />
+ 
+
+</div>
---
+++ api/index.py
@@ -0,0 +1,33 @@
+import os
+import random
+import sys
+import json
+import utils
+
+from collections import namedtuple
+from flask import Flask, jsonify, send_from_directory, abort, send_file, render_template
+
+
+
+app = Flask(__name__)
+
+# Cache all images
+cache_images = [img for img in os.listdir(config.imagefolder)]
+
+# Check for changes in image folder
+extra_dirs = [config.imagefolder, "index"]
+extra_files = extra_dirs[:]
+for extra_dir in extra_dirs:
+    for dirname, dirs, files in os.walk(extra_dir):
+        for filename in files:
+            filename = os.path.join(dirname, filename)
+            if os.path.isfile(filename):
+                extra_files.append(filename)
+
+
+
+
+
+if __name__ == '__main__':
+    utils.randomize(config.imagefolder, config.suffix)
+    app.run(port=config.port, debug=config.debug, extra_files=extra_files)
